d65f1569d4b1197df8874b7892a9d1a5d86885f3
Evan Rourke <evan.rourke.er@gmail.com>
2019-03-07 10:54:30 -0500

    Add a winky face

--- demo.js
+++ demo.js
@@ -2,5 +2,5 @@ function changeColor(){
     // console.log("Hello")
     var x = document.getElementById("p2");
     x.style.color = "blue";
-    x.innerHTML = "-Evan";
+    x.innerHTML = "-Evan ;)";
 }
