9407e92499c1d83c30d89cb02620c8fbf7f5c5ce
Evan <evan.rourke.er@gmail.com>
2021-10-10 16:10:00 -0400

    init vue

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>rawrke's site</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #737a81;
+  margin-top: 50%;
+}
+h1 {
+  border-bottom: 3px double #3e4750;
+  display: inline-flex;
+  padding-bottom: 5px;
+}
+body {
+  background-color: rgb(28, 33, 34);
+}
+</style>
